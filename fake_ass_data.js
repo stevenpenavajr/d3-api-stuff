82c09ff8c80b797e4486ad98376eca60fc730443
Deavin Hester <deavinhester@uky.edu>
2018-10-20 21:37:35 -0400

    fake news simulator with pay

--- fake_ass_data.js
+++ fake_ass_data.js
@@ -1,10 +1,44 @@
-const util = require('util')
+'use strict'
+
+const ilp = require('ilp')
+const spsp = require('ilp-protocol-spsp')
+const debug = require('debug')('ilp-spsp')
+
+// recipient is the payment pointer
+// amount is 1 XRP = 10^9 units
+async function pay (recipient, amount) {
+  try {
+    const plugin = ilp.createPlugin()
+    debug('connecting plugin')
+    await plugin.connect()
+
+    debug('sending payment')
+    await spsp.pay(plugin, {
+      receiver: recipient,
+      sourceAmount: amount
+    })
+  } catch (e) {
+    console.error(e)
+    process.exit(1)
+  }
+
+  console.log('sent!')
+  process.exit(0)
+}
+
+// sending 0.0000001 XRP
+function run (xrp) {
+  pay('$deavin.localtunnel.me', xrp)
+}
+
+
 
 var hour = 17,
 	min = 30,
 	currentRead = 6348;
 
-for (var i=0; i < 60; i++) {
+var a = 5
+for (var i=0; i < a; i++) {
 
 	var rateAmount = Math.random() * Math.floor(20) + 10	// (0-1) * max + min
 	if (22 < rateAmount <= 30)
@@ -26,20 +60,28 @@ for (var i=0; i < 60; i++) {
 	var sub2 = (meterDelta - sub1) * Math.random()
 	var sub3 = (meterDelta - sub1) - sub2
 
-	console.log('dateTime: ' + dateTime)
-	console.log('priceType: ' + priceType)
-	console.log('rateAmount: ' + rateAmount.toString())
-	console.log('rateUnit: ' + rateUnit)
-	console.log('meterNo: ' + meterNo.toString())
-	console.log('currentRead: ' + currentRead.toString())
-	console.log('sub1: ' + sub1.toString())
-	console.log('sub2: ' + sub2.toString())
-	console.log('sub3: ' + sub3.toString() + '\n')
+	//console.log('dateTime: ' + dateTime)
+	//console.log('priceType: ' + priceType)
+	//console.log('rateAmount: ' + rateAmount.toString())
+	//console.log('rateUnit: ' + rateUnit)
+	//console.log('meterNo: ' + meterNo.toString())
+	//console.log('currentRead: ' + currentRead.toString())
+	//console.log('sub1: ' + sub1.toString())
+	//console.log('sub2: ' + sub2.toString())
+	//console.log('sub3: ' + sub3.toString() + '\n')
 
 	min += 1
 	if (min >= 60) {
 		hour += 1
 		min = 0
 	}
+		
+	var xrp = 0.45
+	var moneyOwned = (meterDelta * rateAmount * xrp) * Math.pow(10,9)
+
+	console.log(moneyOwned)
+	run(moneyOwned)
+
+
 
 }
